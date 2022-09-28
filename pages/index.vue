cfceca518340a8ddbbade2fd4ff0c985650dfdae
Daniel Pinto <dmp593@gmail.com>
2022-09-28 12:53:27 +0100

    Worksheet 2

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <b-container>
+        <!-- try to remove :fields=”fields” to see the magic -->
+        <b-table striped over :items="students" :fields="fields" />
+    </b-container>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            fields: ['username', 'name', 'email'],
+            students: []
+        }
+    },
+    created () {
+        this.$axios.$get('/api/students')
+            .then((students) => { this.students = students })
+    }
+}
+</script>
